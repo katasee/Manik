ca76ec6bba359debc0f3aba867b87a58b4490038
Anton K <antk@traffter.com>
2026-07-26 22:10:03 +0200

    Add ScheduleMetrics constants for create-slot popup

--- Manik/Manik/Master/Schedule/ScheduleMetrics.swift
+++ Manik/Manik/Master/Schedule/ScheduleMetrics.swift
@@ -11,4 +11,12 @@ enum ScheduleMetrics {
         static let rowSpacing: CGFloat = 16
         static let timelineHorizontalPadding: CGFloat = 16
     }
+
+    enum CreatePopup {
+        static let cornerRadius: CGFloat = 20
+        static let cardPadding: CGFloat = 20
+        static let rowSpacing: CGFloat = 16
+        static let dimOpacity: Double = 0.4
+        static let defaultDurationMinutes = 30
+    }
 }
